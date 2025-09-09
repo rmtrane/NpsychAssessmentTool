10dfcf7bfe70abafe2191209535d92b5e3fa1cc5
rmtrane <rmtrane@gmail.com>
2025-09-09 11:34:58 -0500

    Changed how to handle get_image_uri

--- inst/qmd/carousel.R
+++ inst/qmd/carousel.R
@@ -91,34 +91,3 @@ navButton <- function(targetId, type, text) {
     span(class = "visually-hidden", text)
   )
 }
-
-
-get_image_uri <- function(file) {
-  image_raw <- lapply(file, FUN = function(x) {
-    readBin(con = x, what = "raw", n = file.info(x)$size)
-  })
-
-  vapply(
-    seq_along(image_raw),
-    FUN.VALUE = character(1L),
-    USE.NAMES = FALSE,
-    FUN = function(x) {
-      file <- file[x]
-      pos <- regexpr("\\.([[:alnum:]]+)$", file)
-      extension <- tolower(ifelse(pos > -1L, substring(file, pos + 1L), ""))
-
-      file <- switch(
-        extension,
-        svg = "image/svg+xml",
-        jpg = "image/jpeg",
-        paste("image", extension, sep = "/")
-      )
-      paste0(
-        "data:",
-        file,
-        ";base64,",
-        base64enc::base64encode(image_raw[[x]])
-      )
-    }
-  )
-}
